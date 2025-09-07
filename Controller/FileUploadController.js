d886a361d53034ad1be0af64ccd69c7109888f8c
Aryaa-7 <aryanojha07@gmail.com>
2025-09-07 09:19:58 +0530

    fix testing

--- Controller/FileUploadController.js
+++ Controller/FileUploadController.js
@@ -50,7 +50,7 @@ export const createStamp = synchFunc(async (req, res) => {
           });
 
           // Upload to /images on your server
-          const remotePath = `/images/${finalFileName}`;
+          const remotePath = `/assets/${finalFileName}`;
           await sftp.put(buffer, remotePath);
           await sftp.end();
 
