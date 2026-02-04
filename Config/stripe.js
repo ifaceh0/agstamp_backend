f2fe3e47d957237a6e0cf212ba9bab2db1d8cfc5
Interfacehub0 <ifaceh0@outlook.com>
2026-02-04 14:42:45 +0530

    Update stripe.js

--- Config/stripe.js
+++ Config/stripe.js
@@ -10,12 +10,25 @@
 
 // export default stripe;
 
+//import Stripe from 'stripe';
+//import dotenv from 'dotenv';
+//import path from 'path';
+
+// Configure dotenv
+//dotenv.config({path: path.join(path.resolve(), "/Config/config.env")});
+
+// Initialize Stripe with your secret key
+//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+//export default stripe;
 import Stripe from 'stripe';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// Configure dotenv
-dotenv.config({path: path.join(path.resolve(), "/Config/config.env")});
+// Configure dotenv (only in development)
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({path: path.join(path.resolve(), "/Config/config.env")});
+}
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
