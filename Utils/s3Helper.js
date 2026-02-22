2d81bc707107d842adf840e952f2ed541f9cc143
sachin <sachinpradhan0719@gmail.com>
2026-02-22 17:06:30 +0530

    added code for lambda deploy

--- Utils/s3Helper.js
+++ Utils/s3Helper.js
@@ -7,8 +7,8 @@ import { ErrorHandler } from "./ErrorHandler.js";
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   },
 });
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import { app } from "./app.js";
-import { dbCon } from "./Config/dbcon.js";
+// import { app } from "./app.js";
+// import { dbCon } from "./Config/dbcon.js";
 
 
 
-//db connection
-dbCon();
+// //db connection
+// dbCon();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`application running on port no>  http://localhost:${process.env.PORT}`)
-})
+// app.listen(process.env.PORT,()=>{
+//     console.log(`application running on port no>  http://localhost:${process.env.PORT}`)
+// })
 
 // import { app } from "./app.js";
 // import { dbCon } from "./Config/dbcon.js";
@@ -25,13 +25,24 @@ app.listen(process.env.PORT,()=>{
 // })
 
 
-// // updated code for lambda deployment
-// import serverless from "serverless-http";
-// import { app } from "./app.js";
-// import { dbCon } from "./Config/dbcon.js";
 
-// // Connect DB once (cold start)
-// await dbCon();
+// updated code for lambda deployment
+import { app } from "./app.js";
+import { dbCon } from "./Config/dbcon.js";
+import serverless from "serverless-http";
 
-// // Export handler for Lambda
-// export const handler = serverless(app);
+let isConnected = false;
+
+const connectDB = async () => {
+  if (!isConnected) {
+    await dbCon();
+    isConnected = true;
+  }
+};
+
+const handler = serverless(app);
+
+export const main = async (event, context) => {
+  await connectDB();
+  return handler(event, context);
+};
